ae682d1cf05f56da75d9969a4988671dabad149f
Sheila Noriega <324032952@C-DNY5WR1.tdsb.on.ca>
2017-02-21 11:21:59 -0500

    My first push -  hello world

---
+++ HelloWorldFirstProject/src/noriegaMestanza/HelloWorld.java
@@ -0,0 +1,16 @@
+package noriegaMestanza;
+/**
+ * HelloWorld.java
+ * Prints out hello world onto console
+ * February 21st, 2017
+ * @author 324032952 Sheila Noriega Mestanza
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello, World!");
+	}
+
+}
